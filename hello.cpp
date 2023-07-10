62550a43ccdcf81f54e958aa4646f5401bef7b79
pamawasada <139094561+pamawasada@users.noreply.github.com>
2023-07-10 10:46:25 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+	{  cout << "Student Code : 6606021620201" << endl;
+	   cout << "Student Name : Awasada Haporn" << endl;
+	   cout << "    Division : Information Technology" << endl;
+	   cout << "        Year : 1" << endl;
+	   cout << "        Room : RA" << endl;
+	   cout << "        Sex  :  Female" << endl;
+	   cout << "        Age  : 18" << endl;
+   cout<<"sec3"<<endl;
+   system("pause");
+   return(0);
+} 
